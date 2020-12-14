44dffe8f0369efb8ed417a992581cd6b951a73ab
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-12-14 20:27:03 +0100

    Add Authority class and update User

---
+++ src/main/java/com/matkoc/library/security/Authority.java
@@ -0,0 +1,23 @@
+package com.matkoc.library.security;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="authorities")
+public class Authority{
+
+    @Id
+    @Column(name="username")
+    private String username;
+
+    @Column(name="authority")
+    private String authority;
+
+
+    public String getAuthority(){
+        return authority;
+    }
+}
--- src/main/java/com/matkoc/library/security/User.java
+++ src/main/java/com/matkoc/library/security/User.java
@@ -1,5 +1,33 @@
 package com.matkoc.library.security;
 
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name="users")
 public class User{
+
+    @Id
+    @Column(name="username")
+    String username;
+    @Column(name="password")
+    String password;
+
+    @OneToMany(
+            fetch = FetchType.EAGER, cascade = CascadeType.ALL,
+            mappedBy = "username"
+    )
+    List<Authority> authorities;
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public List<Authority> getAuthorities(){
+        return authorities;
+    }
 }
