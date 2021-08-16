f4bb76646952b78fa6833634f8de6f9f24e1480f
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-08-16 20:44:25 +0200

    login

--- src/main/java/com/matkoc/library/security/SecurityConfiguration.java
+++ src/main/java/com/matkoc/library/security/SecurityConfiguration.java
@@ -41,6 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
             .authorizeRequests()
+            .antMatchers("/js/**", "/css/**").permitAll()
             .antMatchers("/librarian/**").hasRole("LIBRARIAN")
             .antMatchers("/reader/**").hasRole("READER")
             .antMatchers("/login/activate-account").hasAnyRole("INACTIVE", "INACTIVE_LIB")
